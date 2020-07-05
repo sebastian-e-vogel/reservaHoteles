3ce941e975524fb3bfa9acad3abfbfee59900f27
Sebastian Vogel <sebastian.e.vogel@live.com>
2020-07-05 18:01:43 -0300

    added condiciotal, date of entry can't be minor than today

--- scripts/app.js
+++ scripts/app.js
@@ -32,8 +32,6 @@ class App extends React.Component {
 
   // funcion para aplicar los filtros seleccionados en los inputs.
   filterHotels = () => {
-        debugger;
-
     //filtrado por pais
     let hotelsFilteredByCountry = this.state.hotels.filter((hotel) => {
       if (this.state.filters.country === "all") {
@@ -59,13 +57,23 @@ class App extends React.Component {
       }
       return hotel.rooms > 20;
     });
+    debugger;
     //filtrado por fecha de entrada
-    let hotelsFilteredByGetInDate = hotelsFilteredByRooms.filter((hotel) => {
-      return (
-        this.state.filters.availabilityFrom >= hotel.availabilityFrom &&
-        this.state.filters.availabilityFrom <= hotel.availabilityTo
-      );
-    });
+    let hotelsFilteredByGetInDate = [];
+    if (
+      this.state.filters.availabilityFrom < today.valueOf() &&
+      this.state.filters.availabilityFrom
+    ) {
+      alert("la fecha de salida no puede ser anterior a la de entrada");
+      return hotelsFilteredByGetInDate;
+    } else {
+       hotelsFilteredByGetInDate = hotelsFilteredByRooms.filter((hotel) => {
+        return (
+          this.state.filters.availabilityFrom >= hotel.availabilityFrom &&
+          this.state.filters.availabilityFrom <= hotel.availabilityTo
+        );
+      });
+    }
 
     let hotelsFilteredByGetOutDate = [];
     //filtrado por fecha de salida
@@ -75,7 +83,7 @@ class App extends React.Component {
       this.state.filters.availabilityTo
     ) {
       alert("la fecha de salida no puede ser anterior a la de entrada");
-      return (hotelsFilteredByGetOutDate = []);
+      return hotelsFilteredByGetOutDate;
     } else if (hotelsFilteredByGetInDate.length) {
       hotelsFilteredByGetOutDate = hotelsFilteredByGetInDate.filter((hotel) => {
         return this.state.filters.availabilityTo <= hotel.availabilityTo;
